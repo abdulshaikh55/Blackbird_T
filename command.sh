0ec414afd970b6bfb5d27690ef9b1c22b6ea701a
Abdulfaiz Shaikh <116955072+abdulshaikh55@users.noreply.github.com>
2023-07-03 11:31:08 +0530

    command.sh

---
+++ command.sh
@@ -0,0 +1,13 @@
+git init
+git add
+git commit -m "BalckBird"
+git branch
+gh repo new
+git -m "update_logo"
+git branch
+git clone  https://github.com/abdulshaikh55/Blackbird_T/tree/update_logo
+gh repo clone abdulshaikh55/Blackbird_T
+git checkout update_logo
+git push origin master
+git pull origin update_logo
+git push origin update_logo
