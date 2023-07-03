a5b124b06776bcc10bb1ed6b84e797d42f1bd29a
Abdulfaiz Shaikh <116955072+abdulshaikh55@users.noreply.github.com>
2023-07-03 11:36:14 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,3 +1,6 @@
+npx create-react-app base-react-app
+cd base-react-app
+gh auth login
 git init
 git add
 git commit -m "BalckBird"
@@ -11,3 +14,4 @@ git checkout update_logo
 git push origin master
 git pull origin update_logo
 git push origin update_logo
+# repo url: https://github.com/abdulshaikh55/Blackbird_T
